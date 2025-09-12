1a4c9b920c27315f0686732a3bd90b7d76e3c3b2
Alex-Camacho <cuentascriptico@gmail.com>
2025-09-12 17:17:46 +0200

    Añadido css para ajustar tamaño de las celdas al tamaño del container
    Uso de flex: 0 0 calc(100% /8) y height de
    misma manera.

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
 const megacontainer = document.querySelector(".megacontainer");
 
 
-for(i = 0; i < 16; i++){
+for(i = 0; i < 64; i++){
     const newContainer = document.createElement("p");
     newContainer.classList.add("grid");
     megacontainer.appendChild(newContainer);
