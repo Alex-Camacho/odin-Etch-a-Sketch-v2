01253aeb34cbfe9bafabcf8425a875d4c68aa2b9
Alex-Camacho <cuentascriptico@gmail.com>
2025-09-12 17:48:24 +0200

    Añadidos EventListeners para colorear cells de rojo

--- javascript.js
+++ javascript.js
@@ -1,9 +1,18 @@
 //Señalando megacontainer
 const megacontainer = document.querySelector(".megacontainer");
 
-
+//Loop para crear celda 8x8,
+//añadiendo clase .grid a los elementos
 for(i = 0; i < 64; i++){
     const newContainer = document.createElement("p");
     newContainer.classList.add("grid");
     megacontainer.appendChild(newContainer);
 }
+
+//EventListener hover
+const cells = document.querySelectorAll(".grid");
+cells.forEach(cell => {
+    cell.addEventListener("mouseover", () =>{
+    event.currentTarget.style.backgroundColor = "red";
+    })
+})
