9f6f7387a04618934cc4c1e905bd90940601e306
Alex-Camacho <cuentascriptico@gmail.com>
2025-09-14 10:17:56 +0200

    Añadida función para cambiar cuadros al pulsar boton 8x8 y 16x16
    Uso de eventListener en
    if (e.target.classList.contains("grid")) {...

--- javascript.js
+++ javascript.js
@@ -1,18 +1,53 @@
 //Señalando megacontainer
 const megacontainer = document.querySelector(".megacontainer");
 
-//Loop para crear celda 8x8,
-//añadiendo clase .grid a los elementos
+//Loop para crear celda 8x8 añadiendo clase
+//.grid a los elementos, por defecto al iniciar.
 for(i = 0; i < 64; i++){
     const newContainer = document.createElement("p");
-    newContainer.classList.add("grid");
+    newContainer.classList.add("grid", "grid-8");
     megacontainer.appendChild(newContainer);
 }
 
-//EventListener hover
-const cells = document.querySelectorAll(".grid");
-cells.forEach(cell => {
-    cell.addEventListener("mouseover", () =>{
-    event.currentTarget.style.backgroundColor = "red";
+//EventListener hover rojo por defecto
+megacontainer.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("grid")) {
+    e.target.style.backgroundColor = "red";
+  }
+});
+
+
+//Señalando button 16x16, borra cells y crea el número correcto
+
+const button16 = document.querySelector("#button16");
+button16.addEventListener("click", () =>{
+    const cells = document.querySelectorAll(".grid");
+    cells.forEach(cell =>{
+        cell.remove();
     })
+
+    for(i = 0; i < 256; i++){
+    //añadir que cambie el height de .grid
+    const newContainer = document.createElement("p");
+    newContainer.classList.add("grid", "grid-16");
+    megacontainer.appendChild(newContainer);
+}
 })
+
+//Misma acción con button 8x8
+
+const button8 = document.querySelector("#button8");
+button8.addEventListener("click", () =>{
+    const cells = document.querySelectorAll(".grid");
+    cells.forEach(cell =>{
+        cell.remove();
+    })
+
+    for(i = 0; i < 64; i++){
+    //añadir que cambie el height de .grid
+    const newContainer = document.createElement("p");
+    newContainer.classList.add("grid", "grid-8");
+    megacontainer.appendChild(newContainer);
+}
+})
+
